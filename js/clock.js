f760fee507a35b900bb0fcc75a3ec2e31ecd166e
Packstudio <geniuslsenl3@naver.com>
2025-08-07 16:01:24 +0900

    Clock Function Added

---
+++ js/clock.js
@@ -0,0 +1,11 @@
+const clock = document.querySelector("#clock");
+
+// clock.innerText = "lala";
+
+function sayHello() {
+    console.log("hello");
+}
+
+// setInterval(sayHello, 1000);
+
+setTimeout(sayHello, 5000);
